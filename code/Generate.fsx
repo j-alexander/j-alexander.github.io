edb602fca84dc1884e0418e373099ebdf3b4d6bb
Jonathan Leaver <jonathan@jet.com>
2016-12-05 17:57:10 -0500

    configure for generating certain posts from fsx literate

---
+++ code/Generate.fsx
@@ -0,0 +1,24 @@
+﻿#I "packages/FSharp.Compiler.Service/lib/net45"
+#I "packages/FSharpVSPowerTools.Core/lib/net45"
+#I "packages/FSharp.Formatting/lib/net40"
+
+#r "FSharp.CodeFormat.dll"
+#r "FSharp.Literate.dll"
+open FSharp.Literate
+open System.IO
+
+let source name =
+    Path.Combine(__SOURCE_DIRECTORY__, name)
+let target name =
+    Path.Combine(__SOURCE_DIRECTORY__, "..\_posts", name)
+let generate name =
+    let input = source <| sprintf "%s.fsx" name
+    let output = target <| sprintf "%s.html" name
+    Literate.ProcessScriptFile(
+        input=input,
+        output=output,
+        format=OutputKind.Html,
+        lineNumbers=false)
+    
+#r "packages/RdKafka/lib/net451/RdKafka.dll"
+generate "2016-12-08-rdkafka-subscription-in-fsharp"

75c9666b03e542c36fb2246feec21dd8791d8c40
Jonathan Leaver <jonathan@jet.com>
2016-12-21 17:23:31 -0500

    add objective

--- code/2016-12-15-jsonpath-queries-using-fsharpdata.fsx
+++ code/2016-12-15-jsonpath-queries-using-fsharpdata.fsx
@@ -1,5 +1,29 @@
 ﻿(**
 ## Objective
+
+[F#](http://fsharp.org/about/index.html) has been characterized as a modern programming language
+well-suited to data-rich analytical problems.
+In this language, interacting with diverse datasets from event streams, web services, and databases
+is further simplified with the open-source [FSharp.Data](http://fsharp.github.io/FSharp.Data/)
+library.
+
+Many of these real-world documents are hierarchically structured in Json and Xml.
+When designing reusable applications and algorithms in this space, it becomes _very important_ to
+be able to extract subsets of data using a custom path specifier.
+
+For [Xml](https://en.wikipedia.org/wiki/XML), [XPATH](http://www.w3schools.com/xml/xml_xpath.asp) is
+a well established tool for describing specific elements of a document.
+
+A similar mechanism exists for Json, called *[JsonPath](http://goessner.net/articles/JsonPath/)*.
+
+Using JsonPath with F# typically involves converting data to 
+[Newtonsoft.Json](http://www.newtonsoft.com/json/help/html/QueryJsonSelectToken.htm)'s `JObject`
+type and invoking `SelectToken`.  However, FSharp.Data has an elegant and functional representation
+of [JsonValue](http://fsharp.github.io/FSharp.Data/reference/fsharp-data-jsonvalue.html)s that is 
+very widely used.
+
+In this article, I describe _one possible way_ to directly query subsets of a `JsonValue` document
+without converting your entire dataset to perform an extraction.
 *)
 (*** hide ***)
 open System
@@ -7,7 +31,7 @@ open System.Text
 open System.Text.RegularExpressions
 (**
 ## Dependencies
-1. *FSharp.Data* from NuGet, or using Paket:
+1. *FSharp.Data* from [NuGet](https://www.nuget.org/packages/newtonsoft.json/), or using [Paket](https://fsprojects.github.io/Paket/):
 
  ```powershell
    ./.paket/paket.exe add nuget FSharp.Data project MyProject

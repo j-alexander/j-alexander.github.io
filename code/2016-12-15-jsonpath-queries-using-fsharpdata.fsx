da43e77fa57c55f347ceddab345b39d425d014ff
Jonathan Leaver <jonathan@jet.com>
2016-12-22 15:16:31 -0500

    benchmark image and generate tables

--- code/2016-12-15-jsonpath-queries-using-fsharpdata.fsx
+++ code/2016-12-15-jsonpath-queries-using-fsharpdata.fsx
@@ -401,6 +401,16 @@ search, or optionally obtain the first match case:
         findSeq query >> Seq.tryPick Some
 (**
 ### Testing
+
+<img src="benchmarks.png" class="post-slide" alt="Comparison w/Newtonsoft.Json"/>
+
+|Query|Json.NET (JsonValue->string)|Json.NET (string)|JsonValue (string->JsonValue)|JsonValue (JsonValue)|
+|---|---:|---:|---:|---:|
+|$..title (197778 results)|12.99|10.25|11.18|6.82|
+|$..sku_id (708768 results)|12.67|10.04|11.18|6.82|
+|$.source.data.images[1:].md5 (644 results)|12.66|8.99|8.51|4.24|
+|$.no.match (0 results)|11.70|8.65|7.95|3.88|
+
 *)
 (*** hide ***)
 //-- show me how to use it!
